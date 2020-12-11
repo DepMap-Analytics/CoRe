a106178c5b51667c5e1dadf85b3119d54e4f5ea8
francescojm <fi1@mib111831i.home>
2020-12-11 09:58:47 +0000

    stability test added

--- vignette/demoScript.R
+++ vignette/demoScript.R
@@ -79,7 +79,31 @@ data(curated_BAGEL_essential)
 PanCacer_CF_genes<-
   CoRe.PanCancer_AdAM(pancan_depMat = BinDepMat,
                       tissues_ctypes = tissues_ctypes,
-                      clannotation = clannotation,TruePositives = curated_BAGEL_essential,display = FALSE)
+                      clannotation = clannotation,
+                      TruePositives = curated_BAGEL_essential,
+                      display = FALSE)
 
+RES<-systematic_CS_AdAM_res<-lapply(tissues_ctypes[8],function(x){
+  cls<-clannotation$model_name[clannotation$tissue==x | clannotation$cancer_type==x]
+  cls<-intersect(colnames(pancan_depMat),cls)
+  cs_depmat<-pancan_depMat[,cls]
+
+  res<-lapply(10:length(cls),function(x){
+    print(x)
+    do.call(rbind,lapply(1:100,function(y){
+      print(y)
+      ss<-sample(cls,x)
+      CFs<-CoRe.AdAM(cs_depmat[,ss],display = FALSE,verbose = FALSE,
+                     TruePositives = curated_BAGEL_essential)
+
+      atLeastOne<-names(which(rowSums(cs_depmat[,ss])>0))
+      CS<-length(setdiff(atLeastOne,CFs))
+
+      return(data.frame(nCF=length(CFs),nCS=CS))
+      }))
+  })
+  CSspec<-setdiff(names(which(rowSums(cs_depmat)>0)),CFs)
+  return(list(ncls=length(cls),CFs=CFs,CSs=CSspec))
+})
 
 
