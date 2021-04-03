d2183c5d57f7cbb8196067ea483a050b17c2c1d1
Alessandro Vinceti <alessandro.vinceti@fht.org>
2021-04-03 12:24:45 +0200

    packages info

--- setup.py
+++ setup.py
@@ -6,22 +6,4 @@ with open("README.md", "r") as fh:
 with open("requirements.txt") as f:
     requirements = f.read().split("\n")
 
-setuptools.setup(
-    name="CoRe",
-    version="0.1.0",
-    author="Emre Karakoc, Clare Pacini, Alessandro Vinceti and Francesco Iorio",
-    author_email="francesco.iorio@sanger.ac.uk",
-    description="CoRe",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/DepMap-Analytics/CoRe",
-    packages=setuptools.find_packages(),
-    install_requires=requirements,
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
-        "Operating System :: OS Independent",
-    ],
-    python_requires='>=3.6',
-)
-
+pip3 install -r requirements.txt
