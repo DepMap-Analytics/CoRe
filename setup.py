21a2cc1ffd5e34eb672f826a8e9c84c0ddd1a0d4
Alessandro Vinceti <alessandro.vinceti@fht.org>
2021-04-03 12:25:30 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,1 @@
-import setuptools
-
-with open("README.md", "r") as fh:
-    long_description = fh.read()
-
-with open("requirements.txt") as f:
-    requirements = f.read().split("\n")
-
 pip3 install -r requirements.txt
