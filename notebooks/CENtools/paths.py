568edc662dc56eedbb769f6ad2b312c64e539f9f
Alessandro Vinceti <alessandro.vinceti@fht.org>
2021-04-07 12:40:30 +0200

    add

--- notebooks/CENtools/paths.py
+++ notebooks/CENtools/paths.py
@@ -1,7 +1,7 @@
 import os
 
 # Specify the paths to import them from the modules
-path = os.getcwd() + "/CENtools/"
+path = os.getcwd() + "/"
 raw_data_path = path + "data/raw_data/"
 curated_data_path = path + "data/curated_data/"
 object_path = path + "data/objects/"
