4980f969cc111821388324ed5a883552779b1f28
Francesco Iorio <fi1@sanger.ac.uk>
2021-04-12 22:11:33 +0100

    functional characterisation of hits

--- notebooks/Characterising_CFsets.R
+++ notebooks/Characterising_CFsets.R
@@ -1,3 +1,4 @@
+
 my.hypTest<-function(x,k,n,N){
 
 
@@ -68,8 +69,6 @@ enrichedGeneFamilies<-function(geneset,BGgs){
   toTest<-names(toTest)
   toTest<-setdiff(toTest,'(Other)')
 
-  pvals<-vector()
-  GENES<-vector()
 
   RES<-do.call(rbind,lapply(toTest,function(x){
     n<-length(which((unlist(lapply(lapply(tokened,'intersect',x),'length')))>0))
@@ -79,6 +78,7 @@ enrichedGeneFamilies<-function(geneset,BGgs){
     GENES<-paste(sort(currSet),collapse=', ')
     res<-data.frame(pval=pvals,Recall=x/n,Covered=x,n_in_category=n,n_picked=k,n_background=N,GENES=GENES,stringsAsFactors = FALSE)
   }))
+
   rownames(RES)<-toTest
 
   fdr<-100*p.adjust(RES$pval,'fdr')
@@ -86,85 +86,107 @@ enrichedGeneFamilies<-function(geneset,BGgs){
   RES<-cbind(rownames(RES),fdr,RES)
   colnames(RES)[1]<-'Gene Family'
 
+  RES[,1]<-as.character(RES[,1])
   RES<-RES[order(RES$fdr),]
 
   return(RES)
 }
-enrichedPathways<-function(geneset,BGgs,ming=2){
-
-  k<-length(geneset)
-  N<-length(BGgs)
-
-  pvals<-vector()
-  GENES<-vector()
-
-  nt<-length(PATHCOM_HUMAN$PATHWAY)
-  flag<-1
-
-
-  ii<-unlist(lapply(lapply(PATHCOM_HUMAN$HGNC_SYMBOL,is.element,geneset),'sum'))
-  names(ii)<-NULL
-
-  toTest<-which(ii>=ming)
-
-  nt<-length(toTest)
-  testedP<-NULL
-  for (i in 1:nt){
-    print(i)
-    n<-length(intersect(BGgs,PATHCOM_HUMAN$HGNC_SYMBOL[[toTest[i]]]))
-
-    if (n >=ming){
-
-      currSet<-intersect(geneset,PATHCOM_HUMAN$HGNC_SYMBOL[[toTest[i]]])
-      x<-length(currSet)
-
-      pvals[flag]<-my.hypTest(x,k,n,N)
-
-      GENES[flag]<-paste(sort(currSet),collapse=', ')
-      flag<-flag+1
-      testedP<-c(testedP,as.character(PATHCOM_HUMAN$PATHWAY[[toTest[i]]]))
-    }
-  }
-
-
-  id<-which(p.adjust(pvals,'fdr')<0.05)
-
-  names(pvals)<-testedP
-  pvals<-pvals[id]
-  GENES<-GENES[id]
-
-  GENES<-GENES[order(pvals)]
-  pvals<-sort(pvals)
-
-  RES<-cbind(names(pvals),pvals,GENES)
-  RES<-data.frame(RES,stringsAsFactors = FALSE)
-  RES$pvals<-as.numeric(as.character(RES$pvals))
-  colnames(RES)[1]<-'pathway'
-  rownames(RES)<-NULL
-  return(RES)
-}
-
-data(curated_BAGEL_essential)
-
 
 load('data/preComputed/CFs_sets_plus_training.RData')
+load('data/preComputed/CFs_sets.RData')
+
 load('data/screenedGenes.RData')
+
+col_includingTraining=c("#03B2C8","#034DD9","#E6AB02","#1B9E77","#337100","#FC8D62",
+      '#F4CAE4','#800080','#CAB2D6','#BEBADA','#777892')
+names(col_includingTraining)<-names(CFs_sets_plus_training)
 
 CFGs_infos<-lapply(CFs_sets_plus_training,retrievegeneInfo)
 GFs<-lapply(CFs_sets_plus_training,function(x){enrichedGeneFamilies(x,screenedGenes)})
 names(GFs)<-names(CFs_sets_plus_training)
 
+CFGs_infos_no_training<-lapply(CFs_sets,retrievegeneInfo)
+names(CFGs_infos_no_training)<-names(CFs_sets)
 
-pdf('../../Other Paper Results/GF_enrich.pdf',11,15)
-par(mar=c(4,20,0,6))
+NN<-names(CFGs_infos_no_training)
 
-x<-GFs$`Hart 2014`
-x<-x[which(x$fdr<0.05),]
-tt<-barplot(rev(-log10(x$pval)),horiz = TRUE,xlim=c(1,10),log='x',
-            names.arg = rev(x$`Gene Family`),las=2,border = FALSE,
-            xlab='-log10 pval')
-text(rep(1,length(tt)),tt-0.06,rev(x$GENES),pos = 4,cex=0.7)
-dev.off()
+lapply(NN,function(x){
+  tmp<-CFGs_infos_no_training[[x]]
+  tmp<-cbind(rownames(tmp),tmp)
+  colnames(tmp)[1]<-'Gene'
+
+  write.table(tmp,row.names = FALSE,
+              quote=FALSE,
+              sep='\t',file=paste('../../../Other Paper Results/SuppTable1/',x,'_set.tsv',sep=''))
+})
+
+significantOnly<-lapply(NN,function(nn){
+  print(nn)
+  pdf(paste('../../../Other Paper Results/',nn,'_GF_enr.pdf',sep=''),11,15)
+  par(mar=c(4,20,4,6))
+  par(xpd=TRUE)
+
+  x<-GFs[[nn]]
+  x<-x[which(x$fdr<5),]
+  tt<-barplot(rev(-log10(x$pval)),horiz = TRUE,xlim=c(1,10),log='x',
+              names.arg = rev(x$`Gene Family`),las=2,border = FALSE,
+              xlab='-log10 pval',main = paste(nn,'(FDR < 5%)'))
+
+  tex<-rev(x$GENES)
+  nchar<-100
+  toCut<-which(str_length(tex)>nchar)
+  tex[toCut]<-str_sub(tex[toCut],1,nchar)
+  tex[toCut]<-paste(tex[toCut],', ...',sep='')
+  text(rep(1,length(tt)),tt-0.06,tex,pos = 4,cex=0.7)
+  dev.off()
+
+  return(x$`Gene Family`)
+})
+
+always_enriched_CFGs<-Reduce(intersect,significantOnly[1:6])
+
+RecallOfEnrichedFamilies<-do.call(rbind,lapply(always_enriched_CFGs,function(x){
+  unlist(lapply(GFs,function(g){g[x,'Covered']}))
+}))
+
+rownames(RecallOfEnrichedFamilies)<-always_enriched_CFGs
+
+load('data/col_13_distinct.RData')
+par(mar=c(15,4,4,2))
+par(xpd=TRUE)
+barplot(RecallOfEnrichedFamilies,las=2,col=col_13_distinct,ylim=c(0,400),ylab='n. genes',main='Coverage of gene families
+        always enriched in CFGs (FDR < 5%)')
+plot(0,0,col=NA,frame.plot=FALSE,xlab='',ylab='',xaxt='n',yaxt='n')
+legend('center',rownames(RecallOfEnrichedFamilies),fill=col_13_distinct,title='gene families always enriched in CFGs (FDR < 5%)')
+
+always_enriched_CEGs<-Reduce(intersect,significantOnly[7:11])
+
+allGinfos<-retrievegeneInfo(screenedGenes)
+
+RecallOfEnrichedFamilies_CEGs<-do.call(rbind,lapply(always_enriched_CEGs,function(x){
+  genes_in_the_family<-rownames(allGinfos)[grep(x,allGinfos[,'family'])]
+  unlist(lapply(CFs_sets_plus_training,function(y){length(intersect(y,genes_in_the_family))}))
+}))
+
+rownames(RecallOfEnrichedFamilies_CEGs)<-always_enriched_CEGs
+colnames(RecallOfEnrichedFamilies_CEGs)<-names(CFs_sets_plus_training)
+
+load(file='data/col_57_distinct.RData')
+par(mar=c(15,4,6,2))
+par(xpd=TRUE)
+barplot(RecallOfEnrichedFamilies_CEGs,las=2,col=col_57_distinct,
+  ylim=c(0,800),ylab='n. genes',main='Coverage of gene families always enriched in CEGs (FDR < 5%)',
+  border=NA)
+par(mar=c(4,2,3,2))
+plot(0,0,col=NA,frame.plot=FALSE,xlab='',ylab='',xaxt='n',yaxt='n')
+legend('center',rownames(RecallOfEnrichedFamilies_CEGs),cex=0.55,
+       fill=col_57_distinct,title='gene families always enriched in CEGs (FDR < 5%)',border = NA)
+
+####################################################################################################
+
+
+
+
 
 
 
